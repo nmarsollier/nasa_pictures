66be417aced6039b87b62e3b7fb12781cac3fab2
Nestor Marsollier <nestor.marsollier@deptagency.com>
2024-11-05 13:22:36 -0300

    fixes

--- app/src/main/java/com/nmarsollier/nasa/common/ui/ErrorView.kt
+++ app/src/main/java/com/nmarsollier/nasa/common/ui/ErrorView.kt
@@ -9,12 +9,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,7 +35,7 @@ fun ErrorView(onClick: () -> Unit) {
             .background(colorResource(id = R.color.blueBackground))) {
 
         Image(
-            painterResource(android.R.drawable.stat_notify_error),
+            rememberVectorPainter(Icons.Filled.Info),
             "",
             colorFilter = ColorFilter.tint(colorResource(R.color.error)),
             modifier = Modifier.size(60.dp)
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.7.2" apply false
+    id("com.android.application") version "8.6.1" apply false
     id("com.android.library") version ("8.1.1") apply (false)
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
     id("com.google.devtools.ksp") version "2.0.21-1.0.26" apply false
 }
